1d4b76a3073fb35a6760c5f62f898b14ed467220
Kirylbas <kirill.kolobov.98@gmail.com>
2023-05-26 19:56:04 +0300

    initial commit

--- src/main/java/ProductManager.java
+++ src/main/java/ProductManager.java
@@ -26,10 +26,7 @@ public class ProductManager {
     }
 
     public boolean matches(Product product, String search) {
-        if (product.getName().contains(search)){
-            return true;
-        } else {
-            return false;
+        return  product.getName().contains(search);
         }
     }
-}
+
